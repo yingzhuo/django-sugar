ddde21cb347e07d4848c05c29eef88a4184839cf
应卓 <yingzhor@gmail.com>
2022-08-21 15:51:12 +0800

    update

--- django_sugar/lang/string_tool.py
+++ django_sugar/lang/string_tool.py
@@ -1,4 +1,4 @@
-#
+# ----------------------------------------------------------------------------------------------------------------------
 #  ____                           ____
 # |  _ \(_) __ _ _ __   __ _  ___/ ___| _   _  __ _  __ _ _ __
 # | | | | |/ _` | '_ \ / _` |/ _ \___ \| | | |/ _` |/ _` | '__|
@@ -7,7 +7,10 @@
 #    |__/             |___/                  |___/
 #
 # https://github.com/yingzhuo/django-sugar
-#
+# ----------------------------------------------------------------------------------------------------------------------
+import base64
+
+
 def is_empty(string):
     if string is None:
         return True
@@ -32,3 +35,37 @@ def blank_to_none(string):
         return None
     else:
         return string
+
+
+def none_to_empty(string):
+    if string is None:
+        return ''
+    else:
+        return string
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+def base64_standard_encode(string, charset='utf-8'):
+    string_bytes = string.encode(charset)
+    base64_bytes = base64.standard_b64encode(string_bytes)
+    return base64_bytes.decode(charset)
+
+
+def base64_standard_decode(base64_string, charset='utf-8'):
+    base64_bytes = base64_string.encode(charset)
+    string_bytes = base64.standard_b64decode(base64_bytes)
+    return string_bytes.decode(charset)
+
+
+def base64_urlsafe_encode(string, charset='utf-8'):
+    string_bytes = string.encode(charset)
+    base64_bytes = base64.urlsafe_b64encode(string_bytes)
+    return base64_bytes.decode(charset)
+
+
+def base64_urlsafe_decode(base64_string, charset='utf-8'):
+    base64_bytes = base64_string.encode(charset)
+    string_bytes = base64.urlsafe_b64decode(base64_bytes)
+    return string_bytes.decode(charset)
