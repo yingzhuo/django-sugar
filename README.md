077b748f8d43fe3c2f175a07da4af92386620ba1
应卓 <yingzhor@gmail.com>
2022-08-23 16:52:09 +0800

    update

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # django-sugar
 
+### 安装
+
+```sh
+# install
+pip3 install git+https://github.com/yingzhuo/django-sugar.git
+
+# install (upgrade)
+pip3 install -U git+https://github.com/yingzhuo/django-sugar.git
+```
+
 ### 许可证
 
 [Apache 2](./LICENSE)
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='django_sugar',
+    version='0.1',
+    author='应卓',
+    author_email='yingzhor@gmail.com',
+    url=r'https://github.com/yingzhuo/django-sugar',
+    packages=['django_sugar'],
+    requires=[
+    ],
+)
